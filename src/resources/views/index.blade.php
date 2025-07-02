ae22f4365c67c7150f90533d7f7e3415a763de50
kirin <fnagixjinf38@gmail.com>
2025-07-02 17:07:35 +0900

    20250702

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mogitate</title>
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <a class="header__logo" href="/">
+                mogitate
+            </a>
+        </div>
+    </header>
+
+    <div class="
+</body>
+</html>
