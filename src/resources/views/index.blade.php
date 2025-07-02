f1912a5996d98b4c362d4866208a7d6bcba02581
kirin <fnagixjinf38@gmail.com>
2025-07-02 21:20:43 +0900

    20250702

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -15,6 +15,22 @@
         </div>
     </header>
 
-    <div class="
+    <h1>商品一覧</h1>
+
+    <nav>
+        <ul class="register-nav">
+            <li class="register-nav__item">
+                <a class="register-nav__link" href="/products/register">＋商品を追加</a>
+            </li>
+        </ul>
+    </nav>
+    <form class="search-form">
+        <div class="search-form__item">
+            <input class="search-form__item-input" type="text" />
+        </div>
+        <div class="search-form__button">
+            <button class="search-form__button-submit" type="submit">検索</button>
+        </div>
+    </form>
 </body>
 </html>
